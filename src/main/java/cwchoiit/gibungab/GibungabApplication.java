55742c673a01bce046a67772c3abdfb49e6d65fa
cwchoi <cwchoi@osci.kr>
2026-03-20 08:40:47 +0900

    Init

---
+++ src/main/java/cwchoiit/gibungab/GibungabApplication.java
@@ -0,0 +1,13 @@
+package cwchoiit.gibungab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GibungabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GibungabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cwchoiit/gibungab/GibungabApplicationTests.java
@@ -0,0 +1,13 @@
+package cwchoiit.gibungab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GibungabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
